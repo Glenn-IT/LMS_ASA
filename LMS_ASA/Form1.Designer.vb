05c4397d6e63bdaf62028e6058d099030c82f548
Glenn-IT <glenard2308@gmail.com>
2026-03-29 13:45:32 +0800

    ✅ Development Checklist

--- LMS_ASA/Form1.Designer.vb
+++ LMS_ASA/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class LoginForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -14,18 +14,139 @@ Partial Class Form1
         End Try
     End Sub
 
-    'Required by the Windows Form Designer
     Private components As System.ComponentModel.IContainer
 
-    'NOTE: The following procedure is required by the Windows Form Designer
-    'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        pnlMain = New Panel()
+        lblTitle = New Label()
+        lblSubtitle = New Label()
+        pnlCard = New Panel()
+        lblHeader = New Label()
+        lblUsername = New Label()
+        txtUsername = New TextBox()
+        lblPassword = New Label()
+        txtPassword = New TextBox()
+        btnLogin = New Button()
+        lnkForgotPassword = New LinkLabel()
+        pnlMain.SuspendLayout()
+        pnlCard.SuspendLayout()
+        SuspendLayout()
+
+        ' pnlMain
+        pnlMain.BackColor = Color.FromArgb(31, 78, 121)
+        pnlMain.Dock = DockStyle.Fill
+        pnlMain.Controls.Add(lblTitle)
+        pnlMain.Controls.Add(lblSubtitle)
+        pnlMain.Controls.Add(pnlCard)
+
+        ' lblTitle
+        lblTitle.Text = "Loan Management System"
+        lblTitle.Font = New Font("Segoe UI", 22, FontStyle.Bold)
+        lblTitle.ForeColor = Color.White
+        lblTitle.TextAlign = ContentAlignment.MiddleCenter
+        lblTitle.Size = New Size(760, 50)
+        lblTitle.Location = New Point(20, 60)
+
+        ' lblSubtitle
+        lblSubtitle.Text = "ASA Philippines Foundation, Inc."
+        lblSubtitle.Font = New Font("Segoe UI", 11, FontStyle.Regular)
+        lblSubtitle.ForeColor = Color.FromArgb(180, 220, 255)
+        lblSubtitle.TextAlign = ContentAlignment.MiddleCenter
+        lblSubtitle.Size = New Size(760, 28)
+        lblSubtitle.Location = New Point(20, 112)
+
+        ' pnlCard
+        pnlCard.BackColor = Color.White
+        pnlCard.Size = New Size(380, 370)
+        pnlCard.Location = New Point(210, 158)
+
+        ' lblHeader
+        lblHeader.Text = "Sign In"
+        lblHeader.Font = New Font("Segoe UI", 16, FontStyle.Bold)
+        lblHeader.ForeColor = Color.FromArgb(31, 78, 121)
+        lblHeader.Size = New Size(340, 36)
+        lblHeader.Location = New Point(20, 24)
+        pnlCard.Controls.Add(lblHeader)
+
+        ' lblUsername
+        lblUsername.Text = "Username"
+        lblUsername.Font = New Font("Segoe UI", 9, FontStyle.Regular)
+        lblUsername.ForeColor = Color.Gray
+        lblUsername.Size = New Size(340, 20)
+        lblUsername.Location = New Point(20, 80)
+        pnlCard.Controls.Add(lblUsername)
+
+        ' txtUsername
+        txtUsername.Font = New Font("Segoe UI", 10)
+        txtUsername.Size = New Size(340, 28)
+        txtUsername.Location = New Point(20, 102)
+        txtUsername.BorderStyle = BorderStyle.FixedSingle
+        pnlCard.Controls.Add(txtUsername)
+
+        ' lblPassword
+        lblPassword.Text = "Password"
+        lblPassword.Font = New Font("Segoe UI", 9, FontStyle.Regular)
+        lblPassword.ForeColor = Color.Gray
+        lblPassword.Size = New Size(340, 20)
+        lblPassword.Location = New Point(20, 148)
+        pnlCard.Controls.Add(lblPassword)
+
+        ' txtPassword
+        txtPassword.Font = New Font("Segoe UI", 10)
+        txtPassword.Size = New Size(340, 28)
+        txtPassword.Location = New Point(20, 170)
+        txtPassword.PasswordChar = "●"c
+        txtPassword.BorderStyle = BorderStyle.FixedSingle
+        pnlCard.Controls.Add(txtPassword)
+
+        ' btnLogin
+        btnLogin.Text = "LOGIN"
+        btnLogin.Font = New Font("Segoe UI", 10, FontStyle.Bold)
+        btnLogin.BackColor = Color.FromArgb(31, 78, 121)
+        btnLogin.ForeColor = Color.White
+        btnLogin.FlatStyle = FlatStyle.Flat
+        btnLogin.FlatAppearance.BorderSize = 0
+        btnLogin.Size = New Size(340, 42)
+        btnLogin.Location = New Point(20, 224)
+        btnLogin.Cursor = Cursors.Hand
+        pnlCard.Controls.Add(btnLogin)
+
+        ' lnkForgotPassword
+        lnkForgotPassword.Text = "Forgot Password?"
+        lnkForgotPassword.Font = New Font("Segoe UI", 9)
+        lnkForgotPassword.LinkColor = Color.FromArgb(31, 78, 121)
+        lnkForgotPassword.Size = New Size(340, 20)
+        lnkForgotPassword.Location = New Point(20, 288)
+        lnkForgotPassword.TextAlign = ContentAlignment.MiddleCenter
+        pnlCard.Controls.Add(lnkForgotPassword)
+
+        ' LoginForm
+        Me.AutoScaleDimensions = New SizeF(7F, 15F)
+        Me.AutoScaleMode = AutoScaleMode.Font
+        Me.ClientSize = New Size(800, 580)
+        Me.Controls.Add(pnlMain)
+        Me.Text = "LMS – Login"
+        Me.StartPosition = FormStartPosition.CenterScreen
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+
+        pnlMain.ResumeLayout(False)
+        pnlCard.ResumeLayout(False)
+        pnlCard.PerformLayout()
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents pnlMain As Panel
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents lblSubtitle As Label
+    Friend WithEvents pnlCard As Panel
+    Friend WithEvents lblHeader As Label
+    Friend WithEvents lblUsername As Label
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents lblPassword As Label
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents lnkForgotPassword As LinkLabel
 
 End Class
--- LMS_ASA/Form1.vb
+++ LMS_ASA/Form1.vb
@@ -1,3 +1,15 @@
-﻿Public Class Form1
+﻿Public Class LoginForm
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Dim dashboard As New AdminDashboardForm()
+        dashboard.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub lnkForgotPassword_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles lnkForgotPassword.LinkClicked
+        Dim forgotPw As New ForgotPasswordForm()
+        forgotPw.Show()
+        Me.Hide()
+    End Sub
 
 End Class
